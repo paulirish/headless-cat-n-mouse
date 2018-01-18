4002ccbde1f54d9bd7aeb6e8835507384f85afee
Paul Irish <paul.irish@gmail.com>
2018-01-18 15:32:42 -0800

    Add anti-detection techniques from @sangaline https://intoli.com/blog/not-possible-to-block-chrome-headless/ https://intoli.com/blog/not-possible-to-block-chrome-headless/test-headless-final.js

---
+++ avoid-detection.js
@@ -0,0 +1,50 @@
+
+module.exports = async function(page) {
+  // Pass the User-Agent Test.
+  const userAgent =
+    'Mozilla/5.0 (X11; Linux x86_64)' +
+    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
+  await page.setUserAgent(userAgent);
+
+  // Pass the Webdriver Test.
+  await page.evaluateOnNewDocument(() => {
+    Object.defineProperty(navigator, 'webdriver', {
+      get: () => false
+    });
+  });
+
+  // Pass the Chrome Test.
+  await page.evaluateOnNewDocument(() => {
+    // We can mock this in as much depth as we need for the test.
+    window.chrome = {
+      runtime: {}
+    };
+  });
+
+  // Pass the Permissions Test.
+  await page.evaluateOnNewDocument(() => {
+    const originalQuery = window.navigator.permissions.query;
+    return (window.navigator.permissions.query = parameters =>
+      parameters.name === 'notifications'
+        ? Promise.resolve({state: Notification.permission})
+        : originalQuery(parameters));
+  });
+
+  // Pass the Plugins Length Test.
+  await page.evaluateOnNewDocument(() => {
+    // Overwrite the `plugins` property to use a custom getter.
+    Object.defineProperty(navigator, 'plugins', {
+      // This just needs to have `length > 0` for the current test,
+      // but we could mock the plugins too if necessary.
+      get: () => [1, 2, 3, 4, 5]
+    });
+  });
+
+  // Pass the Languages Test.
+  await page.evaluateOnNewDocument(() => {
+    // Overwrite the `plugins` property to use a custom getter.
+    Object.defineProperty(navigator, 'languages', {
+      get: () => ['en-US', 'en']
+    });
+  });
+};
