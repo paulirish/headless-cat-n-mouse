5a4ea1873a8f0f52af9c97dc19f48db91403466d
Paul Irish <paul.irish@gmail.com>
2018-01-18 15:32:05 -0800

    Add detection techniques from @antoinevastel http://antoinevastel.github.io/bot%20detection/2018/01/17/detect-chrome-headless-v2.html

---
+++ detect-headless.js
@@ -0,0 +1,35 @@
+module.exports = function() {
+  let isHeadless = false;
+
+  function confirmDetection(msg) {
+    console.log(msg);
+    isHeadless = true;
+  }
+
+  if (/HeadlessChrome/.test(window.navigator.userAgent)) {
+    confirmDetection('Chrome headless detected via userAgent');
+  }
+
+  if (navigator.webdriver) {
+    confirmDetection('Chrome headless detected via navigator.webdriver');
+  }
+
+  if (/Chrome/.test(window.navigator.userAgent) && !window.chrome) {
+    confirmDetection('Chrome headless detected via window.chrome');
+  }
+
+  (async () => {
+    const permissionStatus = await navigator.permissions.query({name: 'notifications'});
+    if (Notification.permission === 'denied' && permissionStatus.state === 'prompt') {
+      confirmDetection('Chrome headless detected via permissions API');
+    }
+  })();
+
+  if (navigator.plugins.length === 0) {
+    confirmDetection('Chrome headless potentially detected via navigator.plugins');
+  }
+
+  if (navigator.languages === '') {
+    confirmDetection('Chrome headless detected via navigator.languages');
+  }
+};
